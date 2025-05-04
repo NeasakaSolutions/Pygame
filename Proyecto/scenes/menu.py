e1b551f709918c7806a9cda442c37666ea87d159
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-05-04 11:04:41 -0400

    Se actualizo el menu para que solo tenga musica en esa ventana y sean botones en vez de imagenes

--- Proyecto/scenes/menu.py
+++ Proyecto/scenes/menu.py
@@ -1,82 +1,100 @@
 # IMportaciones:
 import pygame
 import sys
-import os
 from pygame.locals import QUIT
 from scenes.escena1 import Escena1
-# Importacion de las variales globales
 from settings import ancho, alto, fps
 
-
-# Sentencias para la utilizacion correcta de pygame:
+# Sentencia para funcionamiento de pygame
 pygame.init()
-# Para los efectos de sonido
+# Sentencia para funcionamiento de la musica con pygame
 pygame.mixer.init()
 
-# Creacion de la clase menu:
-class Menu():
-    '''Clase que muestra la configuracion del menu'''
+# Clase menu (Ventana principal):
+class Menu:
+    '''Clase con las configuraciones de la ventana principal'''
 
-    # Constructor de la clase Menu
+    # Funcion contructor de la clase menu:
     def __init__(self):
-        '''Funcion que crea la interfaz de la ventana'''
+        '''Configuracion de los botones, ventana y musica de fondo'''
 
-        # Creacion de un reloj:
         self.reloj = pygame.time.Clock()
-
-        # Crear la ventana del menu principal:
+        # Tamanio de la ventana:
         self.pantalla = pygame.display.set_mode((ancho, alto))
-        # Nombre de la ventana:
+        # Texto de arriba en la ventana:
         pygame.display.set_caption('Ijole')
 
-        # Configuracion de la musica de fondo:
-        # Detener cualquier otro sonido en otro plano:
-        pygame.mixer.stop()
-        pygame.mixer.music.load('Proyecto/assets/sonidos/musica_fondo_menu.mp3')
-        #pygame.mixer.music.load("/home/kali/Documents/Pygame/Proyecto/assets/sonidos/musica_fondo_menu.mp3")
-        # Reproducir en bucle infinito:
-        pygame.mixer.music.play(-1, 0.0)
+        # Llamando a la funcion de reproducir musica:
+        self.reproducir_musica()
 
-        # Configuracion de la imagen de fondo:
-        # Cargar la imagen:
+        # COnfiguracion del fondo
         self.fondo = pygame.image.load('Proyecto/assets/imagenes/fondo_menu.jpg').convert()
-        # Tamanio de la imagen:
         self.fondo = pygame.transform.scale(self.fondo, (ancho, alto))
 
         # Configuracion de los botones:
-        self.boton_inicio = pygame.image.load('Proyecto/assets/imagenes/boton_inicio.png').convert_alpha()
-        self.boton_salir = pygame.image.load('Proyecto/assets/imagenes/boton_salir.png').convert_alpha()
-        # Posicion de los botones:
-        self.rect_inicio = self.boton_inicio.get_rect(center = (ancho // 2, 175))
-        self.rect_salir = self.boton_salir.get_rect(center = (ancho // 2, 275))
+        self.boton_ancho = 200
+        self.boton_alto = 50
+        self.boton_inicio_rect = pygame.Rect((ancho // 2 - self.boton_ancho // 2, 175), (self.boton_ancho, self.boton_alto))
+        self.boton_salir_rect = pygame.Rect((ancho // 2 - self.boton_ancho // 2, 275), (self.boton_ancho, self.boton_alto))
 
+        self.fuente = pygame.font.SysFont("arial", 30)
 
-    # Funcion para funcionamiento del menu
+    # Funcion para iniciar la musica de fondo:
+    def reproducir_musica(self):
+        '''Carga e inicia la musica, se configuro asi para detener la musica al cerrar la ventana menu'''
+
+        pygame.mixer.music.load('Proyecto/assets/sonidos/musica_fondo_menu.mp3')
+        pygame.mixer.music.play(-1, 0.0)
+
+    # Funcion para establecer los botones:
+    def dibujar_boton(self, rect, texto, mouse_pos):
+        '''Funcion que configura los botones para que se vean mas perros'''
+
+        if rect.collidepoint(mouse_pos):
+            color = (0, 0, 0)
+            alpha = 255
+        else:
+            color = (0, 0, 0)
+            alpha = 100
+
+        boton_surface = pygame.Surface((rect.width, rect.height), pygame.SRCALPHA)
+        boton_surface.fill((*color, alpha))
+        self.pantalla.blit(boton_surface, rect.topleft)
+
+        texto_surface = self.fuente.render(texto, True, (255, 255, 255))
+        texto_rect = texto_surface.get_rect(center=rect.center)
+        self.pantalla.blit(texto_surface, texto_rect)
+
+    # Funcion para iniciar el menu:
     def run(self):
-        '''Bucle principal del menu'''
-
+        '''Funcion que inicia la ventana del menu y dibuja todas las configuraciones'''
         while True:
-            # Manejo de eventos
+            mouse_pos = pygame.mouse.get_pos()
             for event in pygame.event.get():
                 if event.type == QUIT:
-                    ejecutando = False
-
+                    pygame.quit()
+                    sys.exit()
+                
+                # Comportamiento de los botones
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if self.rect_inicio.collidepoint(event.pos):
+                    if self.boton_inicio_rect.collidepoint(event.pos):
+                        # Detener la musica del menu:
+                        pygame.mixer.music.stop()
                         escena = Escena1()
                         escena.run()
-                    elif self.rect_salir.collidepoint(event.pos):
+                        # Reanudar musica al volver al menu
+                        self.reproducir_musica()
+                    elif self.boton_salir_rect.collidepoint(event.pos):
                         pygame.quit()
                         sys.exit()
 
-            # Imagen:
+            # Area de dibujo:
             self.pantalla.blit(self.fondo, (0, 0))
-            # Botones:
-            self.pantalla.blit(self.boton_inicio, self.rect_inicio)
-            self.pantalla.blit(self.boton_salir, self.rect_salir)
-            # Renderizado actual de la pantalla
-            pygame.display.flip()
+            self.dibujar_boton(self.boton_inicio_rect, "Inicio", mouse_pos)
+            self.dibujar_boton(self.boton_salir_rect, "Salir", mouse_pos)
 
-            # FPS:
-            self.reloj = pygame.time.Clock()
+            pygame.display.flip()
+            self.reloj.tick(fps)
+
+
 
